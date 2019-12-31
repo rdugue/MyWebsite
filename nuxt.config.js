b9a49b3530af10aefda12b11f69d5e561d0146ec
Ralph Dugue <rdugue1@gmail.com>
2019-12-31 18:45:40 -0500

    Restructuring

---
+++ nuxt.config.js
@@ -0,0 +1,69 @@
+
+export default {
+  mode: 'spa',
+  /*
+  ** Headers of the page
+  */
+  head: {
+    title: 'Ralph Dugue',
+    meta: [
+      { charset: 'utf-8' },
+      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
+      { hid: 'description', name: 'description', content: process.env.npm_package_description || '' }
+    ],
+    link: [
+      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }
+    ]
+  },
+  /*
+  ** Customize the progress-bar color
+  */
+  loading: { color: '#fff' },
+  /*
+  ** Global CSS
+  */
+  css: [
+    '@/assets/main.scss'
+  ],
+  /*
+  ** Plugins to load before mounting the App
+  */
+  plugins: [
+    // Doc: https://vue-composition-api-rfc.netlify.com/
+    '@/plugins/composition-api'
+  ],
+  /*
+  ** Nuxt.js dev-modules
+  */
+  buildModules: [
+    // Doc: https://github.com/nuxt-community/eslint-module
+    '@nuxtjs/eslint-module',
+    // Doc: https://typescript.nuxtjs.org/guide/
+    '@nuxt/typescript-build'
+  ],
+  /*
+  ** Nuxt.js modules
+  */
+  modules: [
+    // Doc: https://buefy.github.io/#/documentation
+    'nuxt-buefy',
+    // Doc: https://axios.nuxtjs.org/usage
+    '@nuxtjs/axios'
+  ],
+  /*
+  ** Axios module configuration
+  ** See https://axios.nuxtjs.org/options
+  */
+  axios: {
+  },
+  /*
+  ** Build configuration
+  */
+  build: {
+    /*
+    ** You can extend webpack config here
+    */
+    extend(config, ctx) {
+    }
+  }
+}
---
+++ pages/index.vue
@@ -0,0 +1,72 @@
+<template>
+  <!--<section class="section">
+    <div class="columns is-mobile">
+      <card
+        title="Free"
+        icon="github-circle"
+      >
+        Open source on <a href="https://github.com/buefy/buefy">
+          GitHub
+        </a>
+      </card>
+
+      <card
+        title="Responsive"
+        icon="cellphone-link"
+      >
+        <b class="has-text-grey">
+          Every
+        </b> component is responsive
+      </card>
+
+      <card
+        title="Modern"
+        icon="alert-decagram"
+      >
+        Built with <a href="https://vuejs.org/">
+          Vue.js
+        </a> and <a href="http://bulma.io/">
+          Bulma
+        </a>
+      </card>
+
+      <card
+        title="Lightweight"
+        icon="arrange-bring-to-front"
+      >
+        No other internal dependency
+      </card>
+    </div>
+  </section>-->
+  <div class="wrapper">
+    <div class="trapezoid"></div>
+  </div>
+</template>
+
+<script>
+// import Card from '~/components/Card'
+
+export default {
+  name: 'HomePage',
+
+  components: {}
+}
+</script>
+<style>
+  .wrapper {
+    height: 100%;
+    width: 100%;
+    background: #5455A5;
+    position: relative;
+  }
+
+  .trapezoid {
+    border-bottom: 300px solid #595;
+    border-left: 100px solid transparent;
+    height: 100%;
+    width: 45%;
+    position: absolute;
+    top: 0;
+    right: 0;
+  }
+</style>

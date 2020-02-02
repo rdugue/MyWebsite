8e1f6724bb1eab9888e72c0366d708d28782649a
Ralph <ralph.dugue@adeptmobi.com>
2020-02-02 01:40:46 -0500

    Markdown editor is a go

--- pages/login.vue
+++ pages/login.vue
@@ -1,24 +1,17 @@
 <template>
-  <div class="tile is-ancestor">
-    <div class="tile is-2"></div>
-    <div class="tile is-8 notification is-primary">
+  <div class="columns">
+    <div class="column is-3"></div>
+    <div class="box column is-half is-primary is-vcentered">
       <section>
         <b-field label="Username">
           <b-input v-model="user.username" maxlength="30"></b-input>
         </b-field>
 
         <b-field label="Password">
-          <b-input
-            type="password"
-            v-model="user.password"
-            maxlength="30"
-            password-reveal
-          ></b-input>
+          <b-input type="password" v-model="user.password" maxlength="30" password-reveal></b-input>
         </b-field>
 
-        <b-button type="is-warning" @click="loginUser(user)" outlined
-          >Login</b-button
-        >
+        <b-button type="is-warning" @click="loginUser(user)" outlined>Login</b-button>
       </section>
     </div>
   </div>
@@ -50,3 +43,9 @@ export default Vue.extend({
   }
 });
 </script>
+
+<style>
+.is-primary {
+  background-color: #5455a5;
+}
+</style>
